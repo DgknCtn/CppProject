ccbfdfc161c0ca590ad415fb37a1227fa58bf70e
Dogukan Cetin <eem.dogukancetin@gmail.com>
2024-03-15 11:55:32 +0300

    ATM Projesine başlandı

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int sifre;
+	int secim;
+	int tutar;
+	string onay;
+	double iban;
+	int gonderilecek_miktar;
+	int telefon;
+	int kod;
+	string teyit;
+
+	cout << "** BANKAMİZA HOSGELDİNİZ **" << endl << endl;
+	cout << "Sifre Giriniz" << endl;
+	cin >> sifre;
+    if(sifre==0000)
+    {
+        cout<<"Sifre Dogru"<<endl<<endl;
+        
+cout<<"Lutfen Yapmak İstediginiz İslemi Secin"<<endl<<endl;
+cout<<"Para Yatirmak İcin 1"<<endl<<endl;
+cout<<"Para Cekmek İcin 2"<<endl<<endl;
+cout<<"EFT Yapmak İcin 3"<<endl<<endl;
+
+cin>>secim;
+cout<<endl;
+if(secim==1)
+{
+    
+}
+}
